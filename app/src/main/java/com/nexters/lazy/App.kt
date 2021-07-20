43bd9e18ff2eab77f6d4b1d2460a45bd078ceb96
gowoonchoi <gowoonvv@gmail.com>
2021-07-21 06:46:12 +0900

    chore: di 추가

--- app/src/main/java/com/nexters/lazy/App.kt
+++ app/src/main/java/com/nexters/lazy/App.kt
@@ -2,7 +2,9 @@ package com.nexters.lazy
 
 import android.app.Application
 import com.kakao.sdk.common.KakaoSdk
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
--- app/src/main/java/com/nexters/lazy/ui/LoginActivity.kt
+++ app/src/main/java/com/nexters/lazy/ui/LoginActivity.kt
@@ -2,15 +2,15 @@ package com.nexters.lazy.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import com.kakao.sdk.user.UserApiClient
 import com.nexters.lazy.MainActivity
 import com.nexters.lazy.R
 import com.nexters.lazy.base.BaseActivity
 import com.nexters.lazy.databinding.ActivityLoginBinding
 import com.nexters.lazy.util.KakaoLoginManager
 import com.nexters.presentation.viewmodel.LoginViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class LoginActivity : BaseActivity<ActivityLoginBinding, LoginViewModel>(){
     override val layoutResId = R.layout.activity_login
     override val viewModelClass = LoginViewModel::class
