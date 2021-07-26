26b87021cad2364c75be5e95d5f3fee4182fbb5d
gowoonchoi <gowoonvv@gmail.com>
2021-07-27 03:24:16 +0900

    feat: #3 하단 스크롤 뷰와 버블 피커 연결 작업

--- app/src/main/java/com/nexters/lazy/adapter/HomeAdapter.kt
+++ app/src/main/java/com/nexters/lazy/adapter/HomeAdapter.kt
@@ -1,6 +1,32 @@
 package com.nexters.lazy.adapter
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import android.widget.BaseAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.nexters.lazy.R
 
-class HomeAdapter {
+class HomeAdapter : RecyclerView.Adapter<HomeAdapter.SampleViewHolder>() {
+
+    private val items = mutableListOf<Int>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = SampleViewHolder(parent)
+
+    override fun onBindViewHolder(holder: SampleViewHolder, position: Int) {
+        //..
+    }
+
+    override fun getItemCount() = items.size
+
+    fun notifySample() {
+        items.clear()
+        (1..3).forEach {
+            items.add(it)
+        }
+        notifyDataSetChanged()
+    }
+
+    class SampleViewHolder(parent: ViewGroup) : RecyclerView.ViewHolder(
+        LayoutInflater.from(parent.context).inflate(R.layout.item_home_habit, parent, false),
+    )
 }
--- app/src/main/java/com/nexters/lazy/ui/HomeFragment.kt
+++ app/src/main/java/com/nexters/lazy/ui/HomeFragment.kt
@@ -2,7 +2,11 @@ package com.nexters.lazy.ui
 
 import android.os.Bundle
 import android.view.View
+import com.igalata.bubblepicker.adapter.BubblePickerAdapter
+import com.igalata.bubblepicker.model.BubbleGradient
+import com.igalata.bubblepicker.model.PickerItem
 import com.nexters.lazy.R
+import com.nexters.lazy.adapter.HomeAdapter
 import com.nexters.lazy.base.BaseFragment
 import com.nexters.lazy.databinding.FragmentHomeBinding
 import com.nexters.presentation.viewmodel.HomeViewModel
@@ -11,8 +15,73 @@ class HomeFragment : BaseFragment<FragmentHomeBinding, HomeViewModel>() {
     override val layoutResId = R.layout.fragment_home
     override val viewModelClass = HomeViewModel::class
 
+    private val sampleAdapter by lazy { HomeAdapter() }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        initRecyclerView()
+        loadSample()
+        picker()
+    }
+
+    private fun initRecyclerView() {
+        with(binding.rvMain) {
+            adapter = sampleAdapter
+        }
+    }
+
+    private fun loadSample() {
+        sampleAdapter.notifySample()
+    }
+
+    private fun picker(){
+        binding.picker.adapter = object : BubblePickerAdapter {
+            override val totalCount = 15
+
+            override fun getItem(position: Int): PickerItem {
+                return PickerItem().apply {
+                    gradient = when(position%3){
+                        0->
+                            BubbleGradient(
+                                R.color.white,
+                                R.color.white,
+                                BubbleGradient.VERTICAL
+                            )
+                        1->
+                            BubbleGradient(
+                                R.color.white,
+                                R.color.white,
+                                BubbleGradient.VERTICAL
+                            )
+                        2->
+                            BubbleGradient(
+                                R.color.white,
+                                R.color.white,
+                                BubbleGradient.VERTICAL
+                            )
+                        else->
+                            BubbleGradient(
+                                R.color.white,
+                                R.color.white,
+                                BubbleGradient.VERTICAL
+                            )
+                    }
+                }
+            }
+        }
+        binding.picker.bubbleSize = 8
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.picker.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        binding.picker.onPause()
+
     }
 
     companion object{
