78b53c6778f9d95b7b72e451ecc800173825e1aa
yoonah5959 <yoonah5959@gmail.com>
2021-07-30 23:34:19 +0900

    feat:#4 base toolbar ui 만드는 중

---
+++ app/src/main/java/com/nexters/lazy/base/BaseToolbarActivity.kt
@@ -0,0 +1,30 @@
+package com.nexters.lazy.base
+
+import android.os.Bundle
+import androidx.annotation.CallSuper
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import com.nexters.lazy.BR
+import com.nexters.presentation.base.BaseViewModel
+import kotlin.reflect.KClass
+
+abstract class BaseToolbarActivity<VB : ViewDataBinding, VM : BaseViewModel> : AppCompatActivity() {
+    protected abstract val layoutResId: Int
+    protected abstract val viewModelClass: KClass<VM>
+
+    lateinit var binding: VB
+
+    protected val viewModel: VM by lazy(LazyThreadSafetyMode.NONE) {
+        ViewModelProvider(viewModelStore, defaultViewModelProviderFactory).get(viewModelClass.java)
+    }
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, layoutResId)
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, viewModel)
+    }
+}
